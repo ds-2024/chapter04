f56abae4590e9c6dffca320e047e7a6e86a6dcf8
ds-2024 <yoonds714@nate.com>
2024-01-12 14:18:53 +0900

    Generic 추가

--- src/com/javaex/ex03/MyList.java
+++ src/com/javaex/ex03/MyList.java
@@ -1,5 +1,46 @@
 package com.javaex.ex03;
 
-public class MyList {
+import java.util.Arrays;
+
+public class MyList<T>{//object를 옮겨서 자료형을 바꾸는식 
+	
+	//필드
+	
+		private T[] oArray;
+		private int crtPos;
+		
+		//생성자
+		public MyList() {
+			//원래 만들지 않음 편의 상 가정
+			oArray = (T[])new Object[3];
+			crtPos = 0;
+		}
+		//메소드 일반
+			
+		public void add(T o) {
+		//현재배열개수+1 배열을 새로 만든다.
+		//이전배열의 값을 새배열에 옮긴다.
+		//마지막방에 새주소를 추가한다.
+						
+		//배열새로 만들고 이전거 복사하고 받은거 추가하고 etc 
+		oArray[crtPos] = o;
+	    // crtPos = crtPos + 1;
+		crtPos +=1;
+						
+		}
+
+		public int size() {
+						
+		return crtPos;
+		}
+		
+		public T get(int index) {
+						
+		return oArray[index];// index방 안에 있는 Point주소
+						
+		
+			
+		}
+
 
 }
--- src/com/javaex/ex03/MyListApp.java
+++ src/com/javaex/ex03/MyListApp.java
@@ -1,9 +1,35 @@
 package com.javaex.ex03;
 
+
+
 public class MyListApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//점관리
+		MyList<Point> pList =  new MyList<Point>();
+		Point p01 = new Point(2,2);
+		Point p02 = new Point(12,12);
+		Point p03 = new Point(22,22);
+		
+		pList.add(p01);
+		pList.add(p02);
+		pList.add(p03);
+		System.out.println(pList.toString());
+		
+		//원관리
+		MyList<Circle> cList = new MyList<Circle>();
+		Circle c01 = new Circle(100);
+		Circle c02 = new Circle(200);
+		
+		cList.add(c01);
+		cList.add(c02);
+		
+		System.out.println(cList.toString());
+		
+		
+		
+	
 
 	}
 
